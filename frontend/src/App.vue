9397d89be744d2b438418460637f24e055852ece
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2022-10-01 20:17:02 -0300

    :sparkles: feat: Criacao frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <RouterLink to="/" class="navbar-item"><strong>Loja Pedros</strong></RouterLink>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <RouterLink to="/verao" class="navbar-item">Verão</RouterLink>
+          <RouterLink to="/inverno" class="navbar-item">Inverno</RouterLink>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <RouterLink to="/log-in" class="button is-light">Entrar</RouterLink>
+              <RouterLink to="/carrinho" class="button is-sucess">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Carrinho</span>
+              </RouterLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
