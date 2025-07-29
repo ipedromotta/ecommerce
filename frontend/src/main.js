fb4f3c41b0d6794e9e32d8a00ff7e28e0ad51196
Pedro Motta <76851399+ipedromotta@users.noreply.github.com>
2025-07-29 17:48:49 -0300

    🐳 Add docker

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,12 +7,9 @@ import router from './router'
 import '../node_modules/bulma'
 import axios from 'axios';
 
-// const host = window.location.hostname
-// const port = window.location.port
+// axios.defaults.baseURL = 'http://localhost:8000'
 
-axios.defaults.baseURL = 'http://localhost:8000'
-
-// axios.defaults.baseURL = `http://${host}:${port}` // Produção
+axios.defaults.baseURL = import.meta.env.VITE_API_URL // Produção
 
 const app = createApp(App)
 
